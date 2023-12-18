cb2a3ef9e9d45feb310b92521b5227118b568da3
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2023-12-18 23:53:03 +0300

    0

--- app/src/main/java/com/ozalp/rentacar/Pages/CarDetails.java
+++ app/src/main/java/com/ozalp/rentacar/Pages/CarDetails.java
@@ -2,8 +2,11 @@ package com.ozalp.rentacar.Pages;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.ozalp.rentacar.Models.Car;
 import com.ozalp.rentacar.databinding.ActivityCarDetailsBinding;
 
 public class CarDetails extends AppCompatActivity {
@@ -13,6 +16,26 @@ public class CarDetails extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityCarDetailsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        Car car = (Car) getIntent().getSerializableExtra("car");
+
+        showCarDetails(car);
+
+    }
+
+    private void showCarDetails(Car car) {
+        try {
+            binding.carBrandAndModelTextView.setText(car.getBrandName() + " " + car.getModelName() + " " + car.getModelYear());
+            binding.carPriceTextView.setText(String.valueOf(car.getDailyPrice()) + " TL");
+            binding.gearTypeTextView.setText(car.getGearType());
+            binding.fuelTypeTextView.setText(car.getFuelType());
+            binding.colorTextView.setText(car.getColorName());
+        } catch (Exception e) {
+            System.out.println(e.getLocalizedMessage());
+        }
+    }
+    public void appointmentRequestButton(View view) {
+
     }
 
     ActivityCarDetailsBinding binding;
